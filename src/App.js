dfa8d0569c03faa455bc112898e5afa1c368680d
Stephanie Zhou <stephiez135@gmail.com>
2026-01-01 20:41:02 -0600

    basic functionality

--- src/App.js
+++ src/App.js
@@ -4,13 +4,35 @@ import { useState } from "react";
 
 function App() {
   const [dataSource, setDataSource] = useState(Array.from({length:20}))
+  const [hasMore,setHasMore] = useState(true)
+  const fetchMoreData=()=> {
+    if(dataSource.length < 40) {
+      //MAKING API CALL
+      setTimeout(() => {
+        setDataSource(dataSource.concat(Array.from({length:20})))
+      }, 500)
+    } else {
+      setHasMore(false)
+    }
+    
+  }
   return (
     <div className="App">
-    <InfiniteScroll dataLength={dataSource.length} next={fetchMoreData}>
+    <h3>Title: <b>Infinite Scroll</b></h3>
+    <div id ="parentScrollDiv" style={{height:500, overflow:'auto'}}>
+    <InfiniteScroll 
+      dataLength={dataSource.length} 
+      next={fetchMoreData} 
+      hasMore={hasMore}
+      loader={<p>Loading...</p>}
+      endMessage={<p>All Done!</p>}
+      scrollableTarget="parentScrollDiv"
+      >
       {dataSource.map((item, index)=> {
-        return <div>This is a div #{index + 1} inside InfiniteScroll </div>
+        return <div className="item">This is a div #{index + 1} inside InfiniteScroll </div>
       })}
     </InfiniteScroll>
+    </div>
     </div>
   );
 }
